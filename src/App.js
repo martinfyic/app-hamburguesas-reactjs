2575da183eef8d4269264007ff035ee63598b301
Martin Ferreira <martin.f.yic@gmail.com>
2022-08-10 22:22:36 -0300

    primer commit para enviar desafio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const person = {
+  name: "Martin",
+  lastName: "Ferreira Yic"
+}
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {person.name} {person.lastName}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Curso React Js - Comisión 436645
+        </p>
       </header>
     </div>
   );
