d77ede24d90783bf8f285a7e83944ee2e2252677
Martin Ferreira <martin.f.yic@gmail.com>
2022-08-30 19:54:13 -0300

    Desafío: Catálogo con MAPS y Promises

--- src/utilities/getProducts.js
+++ src/utilities/getProducts.js
@@ -1,14 +1,3 @@
-// export const getDataProd = async () => {
-//     const url = "productsData.json";
-//     try {
-//         const resp = await fetch(url);
-//         const productsInfo = await resp.json();
-//         return productsInfo;
-//     } catch (error) {
-//         console.warn("El pedido a la API fallo");
-//     }
-// };
-
 // lo hago de esta forma para el desafio unicamente
 export const getDataProd = () => {
     const url = "productsData.json";
