da3387f3e5d925da47e2ebf21bac925270be836f
Martin Ferreira <martin.f.yic@gmail.com>
2022-09-24 20:27:11 -0300

    reestructurando firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBrCx7jh_PqyZKCb1k8IQHLi-UOvLsLQbA",
+    authDomain: "curso-reactjs-80588.firebaseapp.com",
+    projectId: "curso-reactjs-80588",
+    storageBucket: "curso-reactjs-80588.appspot.com",
+    messagingSenderId: "308353234918",
+    appId: "1:308353234918:web:6aeb35395e4bd9f4b48827",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
