7503a1d3614d35b16238a38e65387f473e8af663
Martin Ferreira <martin.f.yic@gmail.com>
2022-11-23 16:10:10 -0300

    Change credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBrCx7jh_PqyZKCb1k8IQHLi-UOvLsLQbA",
-    authDomain: "curso-reactjs-80588.firebaseapp.com",
-    projectId: "curso-reactjs-80588",
-    storageBucket: "curso-reactjs-80588.appspot.com",
-    messagingSenderId: "308353234918",
-    appId: "1:308353234918:web:6aeb35395e4bd9f4b48827",
+	apiKey: process.env.APIKEY_DB,
+	authDomain: process.env.AUTHDOMAIN_DB,
+	projectId: process.env.PROJECTID_DB,
+	storageBucket: process.env.STORAGEBUCKET_DB,
+	messagingSenderId: process.env.MESSAGINGSENDERID_DB,
+	appId: process.env.APPID_DB,
 };
 
 // Initialize Firebase
